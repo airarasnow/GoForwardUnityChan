8c3300290feb171758c9d96c5a0097a8f9415f69
アイララスノウ <airara000snow@gmail.com>
2019-07-06 09:37:43 +0900

    Lesson7の課題です。

---
+++ Assets/Scrips/HitPlaySound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour
+{
+
+    private AudioSource sound01;
+    void Start()
+    {
+        //AudioSourceコンポーネントを取得し、変数に格納
+        sound01 = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Cube")
+        {
+            sound01.PlayOneShot(sound01.clip);
+        }
+    }
+}
